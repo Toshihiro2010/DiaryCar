c7a2b5b13fe41cb37ae5c13c7e3d99a8a3777733
patipan_on <s56122201044@ssru.ac.th>
2017-11-15 13:47:48 +0700

    ทดสอบ รับ Data จาก Server OKHTTP3

--- app/src/main/java/com/stecon/patipan_on/diarycar/VerifiedActivity.java
+++ app/src/main/java/com/stecon/patipan_on/diarycar/VerifiedActivity.java
@@ -15,8 +15,21 @@ import android.widget.Toast;
 
 import com.stecon.patipan_on.diarycar.model.MyDateModify;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class VerifiedActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
@@ -38,8 +51,13 @@ public class VerifiedActivity extends AppCompatActivity implements View.OnClickL
     private String strIdCard;
     private String strDateShow;
 
+    //private String strUrl = "http://csclub.ssru.ac.th/s56122201044/csc4202/monitor/php_get_monitor.php";
+    //private String strUrl = "http://128.1.10.62:8081/BenzApi/Data?Date=0311201703112017";
+    private String strUrl = "http://203.146.239.40:8081/BenzApi/Data?Date=0311201703112017";
+
 
     private MyDateModify myDateModify;
+    private JSONObject jsonObject;
 
 
     @Override
@@ -90,12 +108,81 @@ public class VerifiedActivity extends AppCompatActivity implements View.OnClickL
         strMember = edtMember.getText().toString().trim();
         if (strDateShow.equals("") || strIdCard.equals("") || strMember.equals("")) {
             Toast.makeText(this, "กรุณากรอกข้อมูลให้ครบ", Toast.LENGTH_SHORT).show();
+            myTestHttp();
 
         }else{
             Toast.makeText(this, strIdCard + "\n" + strMember + "\n" + strDateShow, Toast.LENGTH_SHORT).show();
         }
 
     }
+
+    private void myTestHttp() {
+
+        Request request = new Request.Builder()
+                .get()
+                .url(strUrl)
+                .build();
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("Check =>", "onFailed");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("Check => ", "onResponse");
+                String dataResponse = response.body().string();
+                int codeResponse = response.code();
+                Log.d("Response data=> ", dataResponse);
+                Log.d("codeResponse => ", codeResponse +"");
+
+                try {
+//                    JSONObject jsonObject = new JSONObject(dataResponse);
+//                    String product_id = jsonObject.getString("product_id");
+//                    Log.d("prpduct id => ", product_id.toString());
+                    final JSONArray jsonArray = new JSONArray(dataResponse);
+                    final String[] iconString = new String[jsonArray.length()];
+                    final String[] titleString = new String[jsonArray.length()];
+                    final String[] priceString = new String[jsonArray.length()];
+                    final String[] brandString = new String[jsonArray.length()];
+                    final String[] sizeStrings = new String[jsonArray.length()];
+                    final String[] curveStrings = new String[jsonArray.length()];
+                    final String[] typeStrings = new String[jsonArray.length()];
+                    final String[] detailStrings = new String[jsonArray.length()];
+
+                    for (int i = 0 ; i < jsonArray.length() ; i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        iconString[i] = jsonObject.getString("image");
+                        titleString[i] = jsonObject.getString("product_name");
+                        priceString[i] = jsonObject.getString("price");
+                        brandString[i] = jsonObject.getString("brand_name");
+                        sizeStrings[i] = jsonObject.getString("size");
+                        curveStrings[i] = jsonObject.getString("curve");
+                        typeStrings[i] = jsonObject.getString("type_name");
+                        detailStrings[i] = jsonObject.getString("product_detail");
+
+                    }
+                    String temp = jsonArray.getJSONObject(0).getString("product_name");
+                    Log.d("temp => ", temp);
+
+                } catch (JSONException e) {
+                    Log.d("JSONException e => ", e.toString());
+                }
+
+                //Chang Json
+
+            }
+        });
+
+
+
+
+
+
+    }
+
 
 
     private void mySetDate() {
